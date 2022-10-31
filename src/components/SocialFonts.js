d1ccd0df77c8b4eaf6049c896051b9e85a377349
janeodey <janeodeys98@gmail.com>
2022-10-31 20:33:13 +0100

    made changes to link pages

--- src/components/SocialFonts.js
+++ src/components/SocialFonts.js
@@ -1,9 +1,12 @@
 import React from "react"
+import slackimg from "../images/slack-1.jpg"
+import github_logo from "../images/github_logo.png"
 
 const SocialFonts = () =>{
     return(
         <section className="slack">
-        <i className="fa fa-slack"></i> <i className="fa fa-github"></i>
+        <img src={slackimg} width="50px"/><img src={github_logo} width="50px"/>
+        
     </section>
     )
 }
