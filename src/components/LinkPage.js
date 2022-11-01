cbc0beae48e1b98d463c72d3d75adce1d7462436
janeodey <janeodeys98@gmail.com>
2022-11-01 12:47:12 +0100

    made changes to links

--- src/components/LinkPage.js
+++ src/components/LinkPage.js
@@ -1,15 +1,23 @@
 import React from "react"
 
 const LinkPage = () =>{
+
+    const Team = "https://training.zuri.team/";
+    const Books = "http://books.zuri.team" 
+    const Python = " https://books.zuri.team/python-for-beginners?ref_id=JaneOdey" 
+    const Pitch = "https://background.zuri.team";
+    const Design = "https://books.zuri.team/design-rules";
     return(
         <section className="links">
-        <ul>
-            <li><a href="https://training.zuri.team/" id="btn__zuri">Zuri Teams</a></li>
-            <li><a href="http://books.zuri.team" id="books">Zuri Books</a></li>
-            <li><a href=" https://books.zuri.team/python-for-beginners?ref_id=JaneOdey" id="book__python">Python Books</a></li>
-            <li><a href="https://background.zuri.team" id="pitch">Background Checks for Coders</a></li>
-            <li><a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a></li>
-        </ul>
+            <a href="." id="btn__zuri">Twitter Links</a>
+            <a href={Team} id="btn__zuri">Zuri Teams</a>
+
+            <a href={Books} id="books" title=" this is where you find books about design and coding">Zuri Books</a>
+
+            <a href={Python} id="book__python" title="where you feature the book as if you were selling it.">Python Books</a>
+
+            <a href={Pitch} id="pitch" title="where you pitch a service for doing background checks on coders.">Background Checks for Coders</a>
+            <a href={Design} id="book__design" title="where you pitch the free design book offered by Zuri.">Design Books</a>
     </section>
     )
 }
--- src/components/SocialProfile.js
+++ src/components/SocialProfile.js
@@ -4,9 +4,9 @@ import profilePicture from "../images/jane.jpg";
 const SocialProfile = () =>{
     return(
         <div className="profile_section">
-          <img src={profilePicture} alt="Jane Odey's Picture" id="profile_img" width="300" height="300"/>
-          <h1 id="twitter">Jane Odey</h1>
-          <div id="slack" style={{visibility: "hidden"}}>JaneOdey</div>
+          <img src={profilePicture} alt="Jane Odey's Picture" id="profile__img" width="300" height="300"/>
+          <h1 id='twitter'>Jane_Odey_</h1>
+          <div id='slack' style={{visibility: "hidden"}}>JaneOdey</div>
       </div>
     )
 }
